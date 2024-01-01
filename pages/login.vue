d4ff43d2bf9eccf96b9b44c1a63b9a1c818fae5d
Asgarov Asgar <asgara@maestro.st>
2024-01-01 19:57:31 +0400

    manually handled error validation

--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { AxiosError } from 'axios';
+
 definePageMeta({
   layout: "centered",
   middleware: ['guest']
@@ -11,20 +13,39 @@ const form = ref({
   password: ''
 })
 
+const errors = ref({
+  email: [],
+  password: []
+})
+
+async function handleLogin() {
+  try {
+    await login(form.value)
+  } catch (err) {
+    console.log(err);
+    if(err instanceof AxiosError && err.response?.status === 422) {
+      errors.value = err.response.data.errors
+      console.log(errors.value);
+    }
+  }
+}
+
 </script>
 
 <template>
   <div class="login">
     <h1>Login</h1>
-    <form @submit.prevent="login(form)">
+    <form @submit.prevent="handleLogin">
       <label>
         <div>Email</div>
         <input type="text" v-model="form.email" />
+        <div class="p-1 text-red-400" v-for="error in errors.email">{{ error }}</div>
       </label>
 
       <label>
         <div>Password</div>
         <input type="password" v-model="form.password" />
+        <div class="p-1 text-red-400" v-for="error in errors.password">{{ error }}</div>
       </label>
       <button class="btn">Login</button>
     </form>
