97c1ecec414cde42f162608c4af8840391f5d3b5
Asgarov Asgar <asgara@maestro.st>
2024-01-03 22:40:28 +0400

    added filtering data using query builder

--- pages/links/index.vue
+++ pages/links/index.vue
@@ -6,18 +6,25 @@ import type { PaginatedResponse, Link } from '@/types'
 definePageMeta({ middleware: ['auth'] })
 
 const data = ref<PaginatedResponse<Link> | null>(null)
-const page = ref(useRoute().query.page || 1)
+
+const queries = ref({
+  page: 1,
+  'filter[full_link]': '',
+  ...useRoute().query
+})
 
 await getLinks()
 const links = computed(() => data.value?.data)
 
-watch(page, async () => {
+watch(queries, async () => {
   getLinks()
-  useRouter().push({query: { page: page.value }})
-})
+  useRouter().push({query: queries.value})
+}, { deep: true })
 
 async function getLinks() {
-  const { data: resData } = await axios.get(`/links?page=${page.value}`)
+  // @ts-expect-error page is number and that's ok
+  const queryString = new URLSearchParams(queries.value)
+  const { data: resData } = await axios.get(`/links?${queryString}`)
   data.value = resData
 }
 
@@ -28,7 +35,7 @@ async function getLinks() {
     <nav class="flex justify-between mb-4 items-center">
       <h1 class="mb-0">My Links</h1>
       <div class="flex items-center">
-        <SearchInput modelValue="" />
+        <SearchInput v-model="queries['filter[full_link]']" />
         <NuxtLink to="/links/create" class="ml-4">
           <IconPlusCircle class="inline" /> Create New
         </NuxtLink>
@@ -78,7 +85,7 @@ async function getLinks() {
       </table>
       <TailwindPagination 
         :data="data"
-        @pagination-change-page="page = $event"
+        @pagination-change-page="queries.page = $event"
       />
       <div class="mt-5 flex justify-center"></div>
     </div>
